f24a88e1c9a8cdeb9ccaf04ae11cb6ef402709f2
Omar Ali <excalibur1987@gmail.com>
2022-03-29 12:31:42 +0300

    fix pre-install

--- pre-install.sh
+++ pre-install.sh
@@ -6,5 +6,9 @@ sudo rm ~/.zshr&& sudo ln ./.zshr ~/.zshr
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget -o- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+sudo curl http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc -o ~/ACCC4CF8.asc
+sudo apt-key add ~/ACCC4CF8.asc
+sudo rm ~/ACCC4CF8.asc
+
+source ./apt-install.sh
