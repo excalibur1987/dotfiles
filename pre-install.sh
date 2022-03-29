3b01c6de76ab9bed47a6e8130666ac8a13b97da1
Omar Ali <excalibur1987@gmail.com>
2022-03-29 12:17:05 +0300

    add symlink creation script

--- pre-install.sh
+++ pre-install.sh
@@ -1,3 +1,8 @@
+sudo rm ~/.gitignore&& sudo ln ./.gitignore ~/.gitignore
+sudo rm ~/.gitconfig&& sudo ln ./.gitconfig ~/.gitconfig
+sudo rm ~/.bashrc&& sudo ln ./.bashrc ~/.bashrc
+sudo rm ~/.zshr&& sudo ln ./.zshr ~/.zshr
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install lts
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
