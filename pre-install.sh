e29223f574355fc062d755de6503aa361bc0e888
Omar Ali <excalibur1987@gmail.com>
2022-03-29 12:36:42 +0300

    add mssql server

--- pre-install.sh
+++ pre-install.sh
@@ -10,3 +10,6 @@ nvm install --lts
 sudo curl http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc -o ~/ACCC4CF8.asc
 sudo apt-key add ~/ACCC4CF8.asc
 sudo rm ~/ACCC4CF8.asc
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
