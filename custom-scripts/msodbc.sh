22ecf5c17ac1fc814d88d1dd6b045149907e593c
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2023-01-05 16:18:54 +0300

    update msodbc to detect system version

--- custom-scripts/msodbc.sh
+++ custom-scripts/msodbc.sh
@@ -1,12 +1,16 @@
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+version=$(($(lsb_release -r -s)))
+version=${version%.*}
 
-sudo curl "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list" > /etc/apt/sources.list.d/mssql-release.list
-
+if [ "$version" -gt "20" ]; then
+    sudo apt-get install odbcinst
+    sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/21.10/prod impish main" | sudo tee /etc/apt/sources.list.d/mssql-release.list
+else
+    curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    sudo curl "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list" > /etc/apt/sources.list.d/mssql-release.list
+    sudo apt-get update
+    sudo ACCEPT_EULA=Y apt-get install -y mssql-tools17
+fi
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
-# optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools17
-# optional: for unixODBC development headers
 sudo apt-get install -y unixodbc-dev
-
-
