505dc7b63d6d977a4affaf5e37a0deb87187010c
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2023-02-26 08:14:14 +0300

    minor fixes

--- symlinks.sh
+++ symlinks.sh
@@ -5,10 +5,10 @@ sudo rm ~/.gitconfig || true
 sudo rm ~/.bashrc || true
 sudo rm ~/.zshrc || true
 sudo rm ~/.czrc || true
-sudo ln ./dot-files/.gitignore ~/.gitignore
-sudo ln ./dot-files/.gitconfig ~/.gitconfig
-sudo ln ./dot-files/.bashrc ~/.bashrc
-sudo ln ./dot-files/.zshrc ~/.zshrc
-sudo ln ./dot-files/.czrc ~/.czrc
+sudo ln $(pwd)/dot-files/.gitignore ~/.gitignore
+sudo ln $(pwd)/dot-files/.gitconfig ~/.gitconfig
+sudo ln $(pwd)/dot-files/.bashrc ~/.bashrc
+sudo ln $(pwd)/dot-files/.zshrc ~/.zshrc
+sudo ln $(pwd)/dot-files/.czrc ~/.czrc
 
 echo "---------------Symlinks Added------------"
