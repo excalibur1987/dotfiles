2cdee71d4b0a3dd1503e2ff185802df2ed7fcbb6
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2023-04-27 15:06:28 +0300

    add mssql snippet

---
+++ snippets/mssql.sh
@@ -0,0 +1,2 @@
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
