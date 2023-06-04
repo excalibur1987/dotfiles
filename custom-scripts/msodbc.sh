5a793f2b927c53438b35fb6a2d8b08dc9af1aeac
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2023-06-04 21:12:26 +0300

    :sparkles: update msodbc script

--- custom-scripts/msodbc.sh
+++ custom-scripts/msodbc.sh
@@ -8,9 +8,7 @@ if [ "$version" -gt "20" ]; then
 else
     curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
     sudo curl "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list" > /etc/apt/sources.list.d/mssql-release.list
-    sudo apt-get update
-    sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
 fi
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
-sudo apt-get install -y unixodbc-dev
+sudo apt-get install -y mssql-tools unixodbc unixodbc-dev
