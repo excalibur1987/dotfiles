a999a4359094ec434abdedfd543d52f56aa450ed
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2024-01-22 13:40:24 +0300

    ✨ **feat(snippets):** add script for Git simulation with Manim
    - :tada: Introduce `git-sim.sh` script for Git simulation using Manim.
    - :package: Install necessary dependencies for Manim and Git-sim.
      - :package: Update system packages (`sudo apt update`).
      - :hammer_and_wrench: Install build essentials, Python development, and multimedia libraries.
      - :loud_sound: Install additional dependencies for audio and video processing.
    - :snake: Install Manim using pip3 (`pip3 install manim`).
    - :sparkles: Install Git-sim using pip3 (`pip3 install git-sim`).

---
+++ snippets/git-sim.sh
@@ -0,0 +1,10 @@
+sudo apt update
+# install needed dependencies for manim
+sudo apt install build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg
+sudo apt-get install sox ffmpeg libcairo2 libcairo2-dev
+sudo apt install libgirepository1.0-dev
+
+# install manim
+pip3 install manim
+# install git-sim
+pip3 install git-sim
