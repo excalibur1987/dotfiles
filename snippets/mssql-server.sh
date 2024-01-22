43ec2f271ce68523b635e1c5a547ca63d00a43fd
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2024-01-22 13:39:30 +0300

    :fire: **chore(snippets):** remove Heroku database pull script and update MSSQL setup
    - :heavy_minus_sign: Remove Heroku database pull script (`heroku-pull-db.sh`).
    - :recycle: Update MSSQL server setup script (`mssql-server.sh`).
      - :file_folder: Change data and backup directories to use environment variable `$HOME`.
      - :hammer: Modify Docker build and run commands for better clarity.

--- snippets/mssql-server.sh
+++ snippets/mssql-server.sh
@@ -1,6 +1,6 @@
-mkdir -p /home/omar/mssql/data
-mkdir -p /home/omar/mssql/backup
+mkdir -p $HOME/mssql/data
+mkdir -p $HOME/mssql/backup
 
-docker build -t custom-mssql-image /home/omar/dotfiles/snippets/mssql-docker
+docker build -t custom-mssql-image $HOME/dotfiles/snippets/mssql-docker
 
-docker run -d --name sql1-custom -p 1433:1433 -v /home/omar/mssql/data:/var/opt/mssql/data/databases -v /home/omar/mssql/backup:/var/opt/mssql/backup custom-mssql-image
+docker run -d --name sql1-custom -p 1433:1433 -v $HOME/mssql/data:/var/opt/mssql/data/databases -v $HOME/mssql/backup:/var/opt/mssql/backup custom-mssql-image
