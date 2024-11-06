8d44e996327bc6ddbccfdd1c3d35ce14a18c60e6
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2024-11-06 08:24:18 +0300

    🔧 chore(git): configure git settings and commitizen
    - Set global git config for pull.rebase, user email and name
    - Configure global gitignore file path
    - Install and initialize commitizen with emoji support

--- post-install-scripts/git.sh
+++ post-install-scripts/git.sh
@@ -4,3 +4,8 @@ git config --global user.email "excalibur1987@gmail.com"
 git config --global user.name "Omar Ali"
 
 git config --global core.excludesFile '~/.gitignore'
+
+
+npm install -g commitizen git-cz
+commitizen init git-cz --save-dev --save-exact --force
+npm install -g cz-emoji
